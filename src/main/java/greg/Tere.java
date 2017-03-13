766b0c94051b42a9a059d53d3fa40b0521d0a1b9
gregnesselmann <gregness@greeny.cs.tlu.ee>
2017-03-13 10:26:22 +0200

    kodutöö

---
+++ src/main/java/greg/Tere.java
@@ -0,0 +1,24 @@
+package greg;
+
+public class Tere {
+
+    private long id;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
