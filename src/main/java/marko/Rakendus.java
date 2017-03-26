c7cb230531b619808faf3996c64463ce13b312c7
MK <mrxkollo@users.noreply.github.com>
2017-03-26 17:29:08 +0300

    HTML templaadi lisamine ja käivitava rakenduse loomine

--- src/main/java/marko/Rakendus.java
+++ src/main/java/marko/Rakendus.java
@@ -1,6 +1,16 @@
 package marko;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Rakendus{
 
 
-public class Rakendus {
+    public static void main(String[] args){
+        System.getProperties().put("server.port", 4215);
+        SpringApplication.run(Rakendus.class, args);
+    }
 }
