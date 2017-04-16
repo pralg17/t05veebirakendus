e5b06d61233eb3fc0243a0452c59e423874223ec
tanelm <tanelmaasalu@gmail.com>
2017-04-16 23:25:50 +0300

    Kodutöö toodete lisamiseks kauplusesse

---
+++ src/main/java/tanel/Tooted.java
@@ -0,0 +1,26 @@
+package tanel;
+
+import javax.persistence.*;
+
+@Table(name="tooted")
+@Entity
+public class Tooted{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public Long id;
+	public String kauplus;
+	public String tooteNimi;
+	public Integer hind;
+	
+	
+	public Tooted(){
+ 		
+ 	}
+
+ 	public Tooted(String kauplus, String tooteNimi, Integer hind){
+		this.kauplus = kauplus;
+		this.tooteNimi = tooteNimi;
+		this.hind = hind;
+	}	
+ 	
+}
